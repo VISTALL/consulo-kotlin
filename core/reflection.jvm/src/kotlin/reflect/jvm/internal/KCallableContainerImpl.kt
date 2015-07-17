octomarat/kotlin

Support KClass.constructors in reflection

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KCallableContainerImpl.kt
@@ -16,6 +16,7 @@
 
 package kotlin.reflect.jvm.internal
 
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.load.java.structure.reflect.classId
@@ -43,6 +44,8 @@ abstract class KCallableContainerImpl : KDeclarationContainer {
 
     abstract val scope: JetScope
 
+    abstract val constructorDescriptors: Sequence<ConstructorDescriptor>
+
     fun findPropertyDescriptor(name: String, signature: String): PropertyDescriptor {
         val properties = scope
                 .getProperties(Name.guess(name))
@@ -63,8 +66,7 @@ abstract class KCallableContainerImpl : KDeclarationContainer {
     }
 
     fun findFunctionDescriptor(name: String, signature: String): FunctionDescriptor {
-        val functions = scope
-                .getFunctions(Name.guess(name))
+        val functions = (if (name == "<init>") constructorDescriptors.toList() else scope.getFunctions(Name.guess(name)))
                 .filter { descriptor ->
                     RuntimeTypeMapper.mapSignature(descriptor) == signature
                 }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KFunctionFromReferenceImpl.kt
@@ -16,6 +16,7 @@
 
 package kotlin.reflect.jvm.internal
 
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.resolve.scopes.JetScope
 import kotlin.jvm.internal.FunctionReference
 import kotlin.reflect.KotlinReflectionInternalError
@@ -63,6 +64,9 @@ object EmptyContainerForLocal : KCallableContainerImpl() {
         get() = fail()
 
     override val scope: JetScope
+        get() = fail()
+
+    override val constructorDescriptors: Sequence<ConstructorDescriptor>
         get() = fail()
 
     private fun fail() = throw KotlinReflectionInternalError("Introspecting local functions is not yet supported in Kotlin reflection")

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KPackageImpl.kt
@@ -16,10 +16,11 @@
 
 package kotlin.reflect.jvm.internal
 
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.load.java.structure.reflect.classId
 import org.jetbrains.kotlin.resolve.scopes.JetScope
 import kotlin.jvm.internal.KotlinPackage
-import kotlin.reflect.*
+import kotlin.reflect.KPackage
 
 class KPackageImpl(override val jClass: Class<*>) : KCallableContainerImpl(), KPackage {
     val descriptor by ReflectProperties.lazySoft {
@@ -30,6 +31,9 @@ class KPackageImpl(override val jClass: Class<*>) : KCallableContainerImpl(), KP
     }
 
     override val scope: JetScope get() = descriptor.memberScope
+
+    override val constructorDescriptors: Sequence<ConstructorDescriptor>
+        get() = emptySequence()
 
     override fun equals(other: Any?): Boolean =
             other is KPackageImpl && jClass == other.jClass

VISTALL/consulo-kotlin

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KCallableContainerImpl.kt
@@ -16,6 +16,7 @@
 
 package kotlin.reflect.jvm.internal
 
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.load.java.structure.reflect.classId
@@ -43,6 +44,8 @@ abstract class KCallableContainerImpl : KDeclarationContainer {
 
     abstract val scope: JetScope
 
+    abstract val constructorDescriptors: Collection<ConstructorDescriptor>
+
     fun findPropertyDescriptor(name: String, signature: String): PropertyDescriptor {
         val properties = scope
                 .getProperties(Name.guess(name))
@@ -63,8 +66,7 @@ abstract class KCallableContainerImpl : KDeclarationContainer {
     }
 
     fun findFunctionDescriptor(name: String, signature: String): FunctionDescriptor {
-        val functions = scope
-                .getFunctions(Name.guess(name))
+        val functions = (if (name == "<init>") constructorDescriptors.toList() else scope.getFunctions(Name.guess(name)))
                 .filter { descriptor ->
                     RuntimeTypeMapper.mapSignature(descriptor) == signature
                 }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KFunctionFromReferenceImpl.kt
@@ -16,6 +16,7 @@
 
 package kotlin.reflect.jvm.internal
 
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.resolve.scopes.JetScope
 import kotlin.jvm.internal.FunctionReference
 import kotlin.reflect.KotlinReflectionInternalError
@@ -63,6 +64,9 @@ object EmptyContainerForLocal : KCallableContainerImpl() {
         get() = fail()
 
     override val scope: JetScope
+        get() = fail()
+
+    override val constructorDescriptors: Collection<ConstructorDescriptor>
         get() = fail()
 
     private fun fail() = throw KotlinReflectionInternalError("Introspecting local functions is not yet supported in Kotlin reflection")

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KPackageImpl.kt
@@ -16,10 +16,11 @@
 
 package kotlin.reflect.jvm.internal
 
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.load.java.structure.reflect.classId
 import org.jetbrains.kotlin.resolve.scopes.JetScope
 import kotlin.jvm.internal.KotlinPackage
-import kotlin.reflect.*
+import kotlin.reflect.KPackage
 
 class KPackageImpl(override val jClass: Class<*>) : KCallableContainerImpl(), KPackage {
     val descriptor by ReflectProperties.lazySoft {
@@ -30,6 +31,9 @@ class KPackageImpl(override val jClass: Class<*>) : KCallableContainerImpl(), KP
     }
 
     override val scope: JetScope get() = descriptor.memberScope
+
+    override val constructorDescriptors: Collection<ConstructorDescriptor>
+        get() = emptyList()
 
     override fun equals(other: Any?): Boolean =
             other is KPackageImpl && jClass == other.jClass